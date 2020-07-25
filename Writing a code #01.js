ed55ed35b220c78f33fb1151935ef0199e962c00
Rocco <52253292+rockengi19@users.noreply.github.com>
2020-07-25 17:49:53 +0200

    Create Writing a code #01.js

---
+++ Writing a code #01.js
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>JavaScript - Writing a code #01</title>
+    </head>
+    <body>
+        <script>
+            alert("Hello World, how are you?");
+
+        </script>
+    </body>
+</html>
